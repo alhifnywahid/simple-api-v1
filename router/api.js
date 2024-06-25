80a96640b148789651dc2bdf6832d423c837937f
alhifnywahid <hifnycarlytosss@gmail.com>
2024-06-25 23:20:21 +0700

    add getAll users

--- router/api.js
+++ router/api.js
@@ -38,6 +38,17 @@ const messages = {
 	},
 };
 
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
 router.get('/adduser', async (req, res) => {
   const { username, password, email, notelepon } = req.query;
 
